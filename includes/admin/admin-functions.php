f2916763ff6313ee6142f2f2c9bf6684a234ca43
Schema.Press <info@schema.press>
2016-09-21 14:12:47 -0500

    Added new function for recursive array search

--- includes/admin/admin-functions.php
+++ includes/admin/admin-functions.php
@@ -78,6 +78,49 @@ function schema_wp_array_flatten($array) {
 
 
 /**
+* Retrieve a post given its title.
+*
+* @link http://wordpress.stackexchange.com/questions/11292/how-do-i-get-a-post-page-or-cpt-id-from-a-title-or-slug/11296#11296
+*
+* @since 1.6
+*
+* @uses $wpdb
+*
+* @param string $post_title Page title
+* @param string $post_type post type ('post','page','any custom type')
+* @param string $output Optional. Output type. OBJECT, ARRAY_N, or ARRAY_A.
+* @return mixed
+*/
+function schema_wp_get_post_by_title($page_title, $post_type = 'post' , $output = OBJECT) {
+    global $wpdb;
+        $post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_title = %s AND post_type= %s", $page_title, $post_type));
+        if ( $post )
+            return get_post($post, $output);
+
+    return null;
+}
+
+
+/**
+ * Recursive array search
+ *
+ * #link http://php.net/manual/en/function.array-search.php
+ *
+ * @since 1.6
+ * @return Returns the key for needle if it is found in the array, FALSE otherwise. 
+ */
+function schema_wp_recursive_array_search( $needle, $haystack ) {
+    foreach($haystack as $key=>$value) {
+        $current_key=$key;
+        if($needle===$value OR (is_array($value) && schema_wp_recursive_array_search($needle,$value) !== false)) {
+            return $current_key;
+        }
+    }
+    return false;
+}
+
+
+/**
  * Get Currencies
  *
  * @since 1.0
