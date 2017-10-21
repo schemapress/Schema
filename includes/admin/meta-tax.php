f3f4baf4d744782084d07083393374132d0da527
Schema.Press <info@schema.press>
2017-10-20 19:33:14 -0500

    Prepare for update 1.6.9.5

--- includes/admin/meta-tax.php
+++ includes/admin/meta-tax.php
@@ -21,13 +21,13 @@ if (is_admin()){
    * configure your meta box
    */
   $config = array(
-    'id' => 'schema_wp_meta_box',			// meta box id, unique per meta box
-    'title' => __('Schema', 'schema-wp'),	// meta box title
-    'pages' => array('category'),			// taxonomy name, accept categories, post_tag and custom taxonomies
-    'context' => 'normal',					// where the meta box appear: normal (default), advanced, side; optional
-    'fields' => array(),					// list of meta fields (can be added by field arrays)
-    'local_images' => false,				// Use local or hosted images (meta box images for add/remove)
-    'use_with_theme' => false				// change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+    'id' => 'schema_wp_meta_box',				// meta box id, unique per meta box
+    'title' => __('Schema', 'schema-wp'),		// meta box title
+    'pages' => array('category', 'post_tag'),	// taxonomy name, accept categories, post_tag and custom taxonomies
+    'context' => 'normal',						// where the meta box appear: normal (default), advanced, side; optional
+    'fields' => array(),						// list of meta fields (can be added by field arrays)
+    'local_images' => false,					// Use local or hosted images (meta box images for add/remove)
+    'use_with_theme' => false					// change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
   );
   
   
---
+++ includes/json/tag.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Tag
+ *
+ * @since 1.6.9.5
+ */
+ 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action('wp_head', 'schema_wp_output_tag');
+/**
+ * The main function responsible for output schema json-ld 
+ *
+ * @since 1.6.9.5
+ * @return schema json-ld final output
+ */
+function schema_wp_output_tag() {
+	
+	if ( is_admin() ) return;
+	
+	// Run only on category pages
+	if ( is_tag() ) {
+		
+		$output = '';
+		
+		$json = schema_wp_get_tag_json();
+		
+		if ($json) {
+			$output .= "\n\n";
+			$output .= '<!-- This site is optimized with the Schema plugin v'.SCHEMAWP_VERSION.' - https://schema.press -->';
+			$output .= "\n";
+			$output .= '<script type="application/ld+json">' . json_encode($json, JSON_UNESCAPED_UNICODE) . '</script>';
+			$output .= "\n\n";
+		}
+		
+		echo $output;
+	}
+}
+
+
+/**
+ * The main function responsible for putting schema array all together
+ *
+ * @param string $type for schema type (example: CollectionPage)
+ * @since 1.6.9.5
+ * @return array json 
+ */
+function schema_wp_get_tag_json() {
+		
+	global $post, $query_string;
+	
+	// debug
+	//echo'<pre>';print_r($query_string);echo'</pre>';exit;
+	
+	$blogPost 	= array();
+	$json 		= array();
+	
+	$secondary_loop = new WP_Query( $query_string );
+	
+	if ( $secondary_loop->have_posts() ):
+	   
+	   // Get the markup data
+	   if ( ! empty($secondary_loop->posts) ) {
+			foreach ($secondary_loop->posts as $schema_post) {
+				$schema_json = get_post_meta( $schema_post->ID, '_schema_json', true );
+				if ( isset($schema_json) ) {
+					$blogPost[] = $schema_json;
+				}		
+			}
+		}
+		
+		wp_reset_postdata();
+			
+		$tag 			= get_the_tags(); 
+		
+		$tag_id 		= intval($tag[0]->term_id); 
+       	$tag_link 		= get_tag_link( $tag_id );
+       	$tag_headline 	= single_tag_title( '', false ) . __(' Tag', 'schema-wp');
+		$sameAs 		= get_term_meta( $tag_id, 'schema_wp_sameAs' );
+
+		$json = array
+       		(
+				'@context' 		=> 'http://schema.org/',
+				'@type' 		=> "CollectionPage",
+				'headline' 		=> $tag_headline,
+				'description' 	=> strip_tags(tag_description()),
+				'url'		 	=> $tag_link,
+				'sameAs' 		=> $sameAs,
+				'hasPart' 		=> $blogPost
+       		);
+				
+	endif;
+	
+	return apply_filters( 'schema_tag_json', $json );
+}
