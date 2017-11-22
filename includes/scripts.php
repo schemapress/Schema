66fe41ea909b44bc1f0c939c1468fc763b09d20e
Schema.Press <info@schema.press>
2017-11-22 16:19:39 -0600

    Do not process function since we don't use it

--- includes/scripts.php
+++ includes/scripts.php
@@ -3,6 +3,8 @@
 /**
  *  Load the frontend scripts and styles
  *
+ * This is not used since there is no scripts to load on front-end
+ *
  *  @since 1.0
  *  @return void
  */
@@ -17,4 +19,4 @@ function schema_wp_frontend_scripts_and_styles() {
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
 }
-add_action( 'wp_enqueue_scripts', 'schema_wp_frontend_scripts_and_styles' );
+//add_action( 'wp_enqueue_scripts', 'schema_wp_frontend_scripts_and_styles' );
